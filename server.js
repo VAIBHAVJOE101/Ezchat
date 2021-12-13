be259f0e1edc9dc29254d0843360b4ed06846ac3
VAIBHAVJOE101 <vaibhav251200@gmail.com>
2021-12-13 10:46:48 +0530

    Updated

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-const PORT = 3000 || process.env.PORT;
+
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
